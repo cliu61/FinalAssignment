5f8151545c75fb787066a0729f49d97a86e823e9
cliu0087 <cliu0087@student.monash.edu>
2022-10-22 20:16:39 +1100

    Code First Setting

---
+++ FinalAssignment/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace FinalAssignment.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FinalAssignment.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FinalAssignment.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
